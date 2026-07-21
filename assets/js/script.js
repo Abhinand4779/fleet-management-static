2c045b39db2175b95324266ea87385bd0eaa2c8b
Abhinand4779 <abhinandsajith77@gmail.com>
2026-07-21 15:59:29 +0530

    added the changes ie sayed by the client and the agent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-﻿const siteHeader = document.getElementById('site-header');
+const siteHeader = document.getElementById('site-header');
 const siteFooter = document.getElementById('site-footer');
 
 function showLoader() {
@@ -66,6 +66,7 @@ function renderHeader() {
       </button>
       <ul class="nav-links" id="main-nav">
         ${navMarkup}
+        <li><a class="btn btn-download" href="/assets/downloads/Linking-Bridges-Company-Profile.pdf" download><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right:6px;vertical-align:middle;"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>Download Profile</a></li>
         <li><a class="btn btn-primary" href="/pages/contact.html">Request a Quote</a></li>
       </ul>
     </div>`;

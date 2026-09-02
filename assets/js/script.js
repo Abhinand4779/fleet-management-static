e911c694072e6b96a7d8fa56b19d84e2347bd41f
Abhinand4779 <abhinandsajith77@gmail.com>
2026-09-02 09:26:12 +0530

    new pdf added

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,7 +66,7 @@ function renderHeader() {
       </button>
       <ul class="nav-links" id="main-nav">
         ${navMarkup}
-        <li style="margin-left: auto;"><a class="btn btn-download" href="javascript:void(0)"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right:6px;vertical-align:middle;"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>Download Profile</a></li>
+        <li style="margin-left: auto;"><a class="btn btn-download" href="/assets/downloads/Linking%20Bridge%20Transportation%20(3).pdf" target="_blank" download="Linking_Bridge_Transportation_Profile.pdf"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-right:6px;vertical-align:middle;"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>Download Profile</a></li>
         <li style="margin-right: -1rem;"><a class="btn btn-primary" href="/pages/contact.html">Request a Quote</a></li>
       </ul>
     </div>`;
